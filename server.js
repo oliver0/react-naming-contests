bcbca89106a004d7316dd6e1d30ee8dee6bd81b9
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-24 14:19:12 -0600

    Import express. Add middleware

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-import config, { nodeEnv } from './config';
+import config from './config';
+import apiRouter from './api';
 
-console.log(config);
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello Express\n');
+});
+
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port', config.port);
+})
