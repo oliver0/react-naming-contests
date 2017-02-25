ca74236f9f2c8af5b2de205293129eb6bd9ed47d
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-25 11:04:40 -0600

    Add key to child element in map call

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <Header message={this.state.pageHeader}/>
         <div>
           {this.props.contests.map(contest =>
-            <ContestPreview {...contest}/>
+            <ContestPreview key={contest.id} {...contest}/>
           )}
         </div>
       </div>
