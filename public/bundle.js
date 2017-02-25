4d969d2db71ff1d651c9fdb0ebba09dfb7beb877
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-25 10:45:07 -0600

    Add Sass file, add styles

--- public/bundle.js
+++ public/bundle.js
@@ -22256,12 +22256,12 @@
 	    { className: "ContestPreview" },
 	    _react2.default.createElement(
 	      "div",
-	      null,
+	      { className: "category-name" },
 	      contest.categoryName
 	    ),
 	    _react2.default.createElement(
 	      "div",
-	      null,
+	      { className: "contest-name" },
 	      contest.contestName
 	    )
 	  );
