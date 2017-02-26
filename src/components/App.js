09cf5f7586f98599ce70e10e18a1cb35da3b75e0
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-26 11:15:55 -0600

    Add file, create ContestList comp, add to App comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
 
 class App extends Component {
   state = {
@@ -12,9 +12,7 @@ class App extends Component {
 
     }
 
-
   componentWillUnmount(){
-  console.log("message");
     //clean timers, listeners
   }
 
@@ -22,11 +20,7 @@ class App extends Component {
     return(
       <div className="App">
         <Header message={this.state.pageHeader}/>
-        <div>
-          {this.state.contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest}/>
-          )}
-        </div>
+        <ContestList contests={this.state.contests} />
       </div>
     );
   }
