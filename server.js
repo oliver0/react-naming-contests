6078a891d01d64250f1a5b8ca3beca6afa57e156
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-25 18:33:09 -0600

    Add serverRender file. Modify server and config

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 
+import './serverRender';
 server.get('/', (req, res) => {
   res.render('index',  {
     content: 'Hello Express and <em>EJS+</em>'
@@ -23,6 +24,6 @@ server.get('/', (req, res) => {
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
 })
