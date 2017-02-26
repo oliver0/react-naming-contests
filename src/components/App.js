85463cc4302d46f17c88607c807b4e9380cc8760
Oliver Alexander <oliveralexander00@gmail.com>
2017-02-26 11:51:44 -0600

    Use HTML5 history API to change url on clicking contest

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 
+const pushState = (obj, url) =>
+  window.history.pushState(obj, '', url);
+
 class App extends Component {
   state = {
     pageHeader: 'Naming Contests',
@@ -15,12 +18,20 @@ class App extends Component {
   componentWillUnmount(){
     //clean timers, listeners
   }
+  fetchContest = (contestId) => {
+    pushState(
+      {currentContestId: contestId},
+      `/contest/${contestId}`
+    );
+  };
 
   render(){
     return(
       <div className="App">
         <Header message={this.state.pageHeader}/>
-        <ContestList contests={this.state.contests} />
+        <ContestList
+          onContestClick = {this.fetchContest}
+          contests={this.state.contests} />
       </div>
     );
   }
