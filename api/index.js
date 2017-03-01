e19e62e83ba8e8327dfcc2c29f2c47eb3a6fe5ae
Oliver Alexander <oliveralexander00@gmail.com>
2017-03-01 12:28:06 -0600

    Remove all references to in memory data

--- api/index.js
+++ api/index.js
@@ -1,23 +1,13 @@
 import express from 'express';
-import data from '../src/testData';
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
 
 router.get('/contests/', (req, res) =>{
-  res.send({
-    contests: contests
-  });
+
 });
 
 
 router.get('/contests/:contestId', (req, res) =>{
-  let contest = contests[req.params.contestId];
-  contest.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate dapibus est sed cursus. Pellentesque lectus lacus, commodo id cursus tincidunt, accumsan sed risus. Curabitur a enim maximus, malesuada nibh eu, porttitor quam. Nam neque nulla, gravida eget justo sit amet, ullamcorper ultricies quam.";
-  res.send(contest);
 });
 
 export default router;
