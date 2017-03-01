924d43721506dc592809c2f94a5ed44e8dd04c4e
Oliver Alexander <oliveralexander00@gmail.com>
2017-03-01 08:54:37 -0600

    Fetch contest info from api when clicked on

--- api/index.js
+++ api/index.js
@@ -7,12 +7,17 @@ const contests = data.contests.reduce((obj, contest) => {
   return obj;
 }, {});
 
-
-
-
-router.get('/contests', (req, res) =>{
-  res.send({contests: contests
+router.get('/contests/', (req, res) =>{
+  res.send({
+    contests: contests
   });
+});
+
+
+router.get('/contests/:contestId', (req, res) =>{
+  let contest = contests[req.params.contestId];
+  contest.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate dapibus est sed cursus. Pellentesque lectus lacus, commodo id cursus tincidunt, accumsan sed risus. Curabitur a enim maximus, malesuada nibh eu, porttitor quam. Nam neque nulla, gravida eget justo sit amet, ullamcorper ultricies quam.";
+  res.send(contest);
 });
 
 export default router;
